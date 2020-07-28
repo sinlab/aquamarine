96eec3b817975a8eab15cd0f0d07f98418ca47d7
sinlab <sin_lab@hotmail.co.jp>
2020-07-28 12:57:11 +0900

    並列処理勉強
    SSEで書いてみた

--- ParallelTest/ParallelTest/main.cpp
+++ ParallelTest/ParallelTest/main.cpp
@@ -6,7 +6,10 @@
 #include "CpuInfo.h"
 #include "Clock.h"
 
-#define CLIP(v) static_cast<uint8_t>((v<0?0:(255<v?255:v)))
+#define CLIP(v) (v)//((v<0?0:(255<v?255:v)))
+
+#include <xmmintrin.h>	// SSE
+//#include <immintrin.h>	// AVX2
 
 int main(int argc, char* argv[]) {
 
@@ -21,8 +24,12 @@ int main(int argc, char* argv[]) {
 	static const int H = 1080;
 	static const int PIX = CH * W * H;
 
-	const float gain = 0.2f;
+	const int32_t gain_nu = 2, gain_de = 10;
+	const float gain = (float)gain_nu / (float)gain_de;
 	const int32_t offset = 10;
+	const __m128i gain_nu128 = _mm_setr_epi32(gain_nu, gain_nu, gain_nu, gain_nu);
+	const __m128i gain_de128 = _mm_setr_epi32(gain_de, gain_de, gain_de, gain_de);
+	const __m128i offset128 = _mm_setr_epi32(offset, offset, offset, offset);
 
 	uint8_t* pixels = new uint8_t[PIX];
 
@@ -38,10 +45,28 @@ int main(int argc, char* argv[]) {
 			for (int y = 0; y < H; ++y) {
 				for (int x = 0; x < W; ++x) {
 					uint8_t* p = &pixels[y * W * CH + x * CH];
+#if 0
+#if 0
+					// ゲインかけてオフセット。素直な実装
 					p[0] = CLIP(p[0] * gain + offset);
 					p[1] = CLIP(p[1] * gain + offset);
 					p[2] = CLIP(p[2] * gain + offset);
 					p[3] = CLIP(p[3] * gain + offset);
+#else
+					// ゲインかけてオフセット。gainを分子分母に分けてfloat演算除去
+					p[0] = CLIP((p[0] * gain_nu) / gain_de + offset);
+					p[1] = CLIP((p[1] * gain_nu) / gain_de + offset);
+					p[2] = CLIP((p[2] * gain_nu) / gain_de + offset);
+					p[3] = CLIP((p[3] * gain_nu) / gain_de + offset);
+#endif
+#else
+					// 上記をSSEで実装。128/32bitで4並列。遅くなってしまう
+					__m128i ans =_mm_add_epi32(_mm_div_epi32(_mm_mullo_epi32(_mm_setr_epi32(p[0], p[1], p[2], p[3]), gain_nu128), gain_de128), offset128);
+					p[0] = CLIP(ans.m128i_i32[0]);
+					p[1] = CLIP(ans.m128i_i32[1]);
+					p[2] = CLIP(ans.m128i_i32[2]);
+					p[3] = CLIP(ans.m128i_i32[3]);
+#endif
 				}
 			}
 			times.push_back(clock.EndMicroSec());
